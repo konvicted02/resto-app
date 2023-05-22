cfa032eb99fd38e09a6f468c54a7ea548b7e005b
Victor <kovic02@gmail.com>
2023-05-22 13:15:07 -0400

    create the web route to the admin

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
+
+});
+
 require __DIR__.'/auth.php';
